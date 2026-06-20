4ae333bc44cce93ebfe6639b6a96aa41dc3fccf1
takatoshi <ryonagi.koweda.0810@gmail.com>
2026-06-20 17:50:56 +0900

    全体保存

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(CategoriesTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
         $this->call(ItemsTableSeeder::class);
         $this->call(Category_itemTableSeeder::class);
     }
